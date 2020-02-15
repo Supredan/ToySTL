373b464cbe7562af66016bcf0cb811ea95720b6d
Supredan <961401327@qq.com>
2020-02-15 21:26:54 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/ToySTL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ToySTL.dir/main.cpp.obj"
+  "ToySTL.pdb"
+  "ToySTL.exe"
+  "ToySTL.exe.manifest"
+  "libToySTL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ToySTL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
